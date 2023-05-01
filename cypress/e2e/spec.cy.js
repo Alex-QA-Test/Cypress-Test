8de8ab803b4e01f1863007fe05800c3b1165bbc9
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-05-01 21:29:59 +0300

    Commit 3

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
+/// <reference types="Cypress" />
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.get('h1').should('text', 'Kitchen Sink')
+    cy.get('a').should('include.text', 'children')
   })
 })
