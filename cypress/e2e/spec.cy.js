33e8e5f094c9971f77b3a2956be0f3ab14fb7f70
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-05-06 13:08:42 +0300

    Update Test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
     cy.get('h1').should('text', 'Kitchen Sink')
-    cy.get('a').should('include.text', 'children22')
+    cy.get('a').should('include.text', 'Querying')
   })
 })
