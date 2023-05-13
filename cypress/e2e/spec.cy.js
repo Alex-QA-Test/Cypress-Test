9e71f1e410347da40c18f27bc3f52823a2409a05
Alex-QA-Test <125302794+Alex-QA-Test@users.noreply.github.com>
2023-05-13 15:12:46 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit(Cypress.env('https://example.cypress.io')
+    cy.visit('https://example.cypress.io')
     cy.get('h1').should('text', 'Kitchen Sink')
     cy.get('a').should('include.text', 'Querying')
   })
