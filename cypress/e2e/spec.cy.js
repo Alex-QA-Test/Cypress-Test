13219a5ea10a47b86a1f610354755857743fae89
Alex-QA-Test <125302794+Alex-QA-Test@users.noreply.github.com>
2023-05-13 17:30:24 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,9 +3,8 @@ const mySecret = Cypress.env('SECRET_VALUE')
 describe('template spec', () => {
   it('passes', () => {
      cy.log(mySecret)
-    cy.log('23454356')
-    cy.visit('https://example.cypress.io')
-    cy.get('h1').should('text', mySecret)
+    cy.visit(mySecret)
+    cy.get('h1').should('text', 'Kitchen Sink')
     cy.get('a').should('include.text', 'Querying') 
   })
 })
