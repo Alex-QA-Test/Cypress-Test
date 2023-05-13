5b0792879ff49aad03a88c7c444808e304df46e0
Alex-QA-Test <125302794+Alex-QA-Test@users.noreply.github.com>
2023-05-13 15:18:00 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="Cypress" />
 describe('template spec', () => {
-  const mySecret = Cypress.env('TEST_URL')
   it('passes', () => {
-    cy.visit(mySecret)
+     const mySecret = Cypress.env('TEST_URL')
+     cy.log(mySecret)
+   /* cy.visit(mySecret)
     cy.get('h1').should('text', 'Kitchen Sink')
-    cy.get('a').should('include.text', 'Querying')
+    cy.get('a').should('include.text', 'Querying') */
   })
 })
