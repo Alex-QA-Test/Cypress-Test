e91315c1e9a2594a3262d474e491bdf45069150e
Alex-QA-Test <125302794+Alex-QA-Test@users.noreply.github.com>
2023-05-13 17:22:56 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,11 @@
 /// <reference types="Cypress" />
-//const mySecret = Cypress.env('TEST_URL')
+const mySecret = Cypress.env('SECRET_VALUE')
 describe('template spec', () => {
   it('passes', () => {
      cy.log(mySecret)
     cy.log('23454356')
     cy.visit('https://example.cypress.io')
-    cy.get('h1').should('text', TEST_URL)
+    cy.get('h1').should('text', mySecret)
     cy.get('a').should('include.text', 'Querying') 
   })
 })
