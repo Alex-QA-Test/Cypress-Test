e146906b71b9648a43e6e446181585fa80c8640c
Alex-QA-Test <125302794+Alex-QA-Test@users.noreply.github.com>
2023-05-13 15:38:13 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  env: { 
-    URL: 'TEST_URL',
-},
   e2e: {
-    env: { 
-    URL: TEST_URL,
-},
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
