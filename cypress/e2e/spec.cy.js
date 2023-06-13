d1c1cded7fa5157378a2a4ba1ea6906eff92b870
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-06-13 23:12:49 +0300

    Update test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 /// <reference types="Cypress" />
-const mySecret = Cypress.env('SECRET_VALUE')
+//const mySecret = Cypress.env('SECRET_VALUE')
 describe('template spec', () => {
   it('passes', () => {
-     cy.log(mySecret)
-    cy.visit(mySecret)
-    cy.get('h1').should('text', 'Kitchen Sink')
-    cy.get('a').should('include.text', 'Querying') 
+     //cy.log(mySecret)
+    cy.visit('https://www.cypress.io/')
+    cy.get('h1').should('text', ' Test. Automate. ')
+    //cy.get('a').should('include.text', 'Querying') 
   })
 })
