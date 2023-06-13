8c4824e170ca5b88afdf2a7f524f313f3215b997
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-06-13 23:19:40 +0300

    Update test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,8 @@ describe('template spec', () => {
   it('passes', () => {
      //cy.log(mySecret)
     cy.visit('https://www.cypress.io/')
-    cy.get('h1').should('text', ' Test. Automate. ')
+    cy.url().should('eq', 'https://www.cypress.io')
+    //cy.get('h1').should('text', ' Test. Automate. ')
     //cy.get('a').should('include.text', 'Querying') 
   })
 })
