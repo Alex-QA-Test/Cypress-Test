a8d0b0f5bbeaf46e05dd541a817497fbbef1bb0c
Alex-QA-Test <125302794+Alex-QA-Test@users.noreply.github.com>
2023-08-05 12:03:30 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,7 +5,7 @@ describe('template spec', () => {
      //cy.log(mySecret)
     cy.visit('https://www.cypress.io/')
     cy.url().should('eq', 'https://www.cypress.io')
-    //cy.get('h1').should('text', ' Test. Automate. ')
+    cy.get('h1').should('text', ' Test. Automate. ')
     //cy.get('a').should('include.text', 'Querying') 
   })
 })
